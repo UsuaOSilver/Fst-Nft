9551092a8dadd2e2a428989f2fc5818b5393b396
napee19 <napee1904@gmail.com>
2022-08-07 13:30:46 -0700

    add state vars, update refund(), mint()

--- Nft.sol
+++ Nft.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract FstNFT is IERC721 {
+contract FstNFT is ERC721 {
     /**
     1. Compatible with ERC-721 standard -  10 points.
     
@@ -24,13 +24,15 @@ contract FstNFT is IERC721 {
     5. No user can mint more than 5 NFTs in a single transaction - 5 points
         
         Hint: You might wanna try method overloading to keep things optimized and DRY here.
-        
-    6. Deployed address is set as “owner” upon deployment and can trigger refund function 
-    which will allow them to receive all the funds collected by the NFT contract from the sale. 
-    - 10 points 
     */
     
-    uint public totalSupply = 100;
+    uint256 public totalSupply = 100;
+    
+    uint256 public constant nftPrice = 10000000000000000; // 0.01 ETH
+    
+    uint256 public constant maxNftPurchase = 5;
+    
+    uint256 public MAX_NFTS;
     
     constructor(address _nft, uint _nftId) {
         nft = IERC721(_nft);
@@ -41,8 +43,24 @@ contract FstNFT is IERC721 {
         require(msg.value >= 0.01 ether, "You must send at least 0.01 ether");
     }
     
-    function refund() onlyOwner public {
-        nft.safeTransferFrom(address(this), )
+    function mintNft(uint numberOfTokens) public payable {
+        require(numberOfTokens <= maxNftPurchase, "You can only mint up to 5 NFTs in a single transaction");
+        require(numberOfTokens <= totalSupply, "You can only mint up to 100 NFTs");
+        require(msg.value >= nftPrice * numberOfTokens, "You must send at least " + nftPrice * numberOfTokens + " ether");
+        for (uint i = 0; i < numberOfTokens; i++) {
+            nft.mint(msg.sender, nftId + i);
+        }
+        totalSupply += numberOfTokens;
+    }
+    
+    /**
+    6. Deployed address is set as “owner” upon deployment and can trigger refund function 
+    which will allow them to receive all the funds collected by the NFT contract from the sale. 
+    - 10 points 
+    */
+    function refund() public onlyOwner {
+        uint balance = address(this).balance;
+        msg.sender.transefer(balance);
     }
     
 }
