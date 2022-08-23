92a1098964d43a4950fd106334ab5ba1ecd1811a
napee19 <napee1904@gmail.com>
2022-08-23 11:04:19 -0700

    echidna path

--- tests/TestNFT.sol
+++ tests/TestNFT.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-import "contracts/NFT.sol";
+import "../contracts/NFT.sol";
 
-contract TestNFT is NFT {
+contract TestNFT is FstNFT {
+    address echidna_caller = msg.sender;
     
-    constructor() public {
-        FstNFT nft = new FstNFT("FstNFT", "FST", "baseURI");
-    }
+    FstNFT nft = new FstNFT("FstNFT", "FST", "baseURI");
     
     function echidna_max_supply_under_100() public view returns (bool) {
         return (totalSupply() <= 100);
